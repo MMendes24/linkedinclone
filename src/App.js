a47c241ecfaeb1730198418700a16266d6e0d6d0
MMendes24 <mmbusiness1@protonmail.com>
2021-01-19 08:09:15 -0600

    Added Sidebar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Header from "./Header"
+import Sidebar from "./Sidebar"
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
         {/*header*/}
         <Header />
         {/*App Body*/}
+        <div className="appBody">
+          <Sidebar />
+        </div>
         {/*Sidebar*/}
         {/*Feed*/}
         {/*Widgets*/}
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,14 @@
 import React from "react"
 import "./headerOptions.css"
+import { Avatar } from "@material-ui/core"
 
-const HeaderOption = ({ Icon, title }) => {
+const HeaderOption = ({ avatar, Icon, title }) => {
     return (
         <div className="headerOption">
-            {Icon && <Icon className="headerOption" />}
+            {Icon && <Icon className="headerOptionIcon" src={avatar}/>}
+            {avatar && 
+                <Avatar className="headerOptionIcon" />
+            }
             <h3 className="headerOptionTitle">{title}</h3>
         
         </div>
